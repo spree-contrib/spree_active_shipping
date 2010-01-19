ca47a8806551a48cac799e611f7b50663ece84d7
David North <davidnorth@railsdog.com>
2010-01-19 14:26:30 +0000

    apply plugin fixes from github.com/djnawara/active_shipping - namespace conflict caused clash with ActiveMerchant

--- vendor/plugins/active_shipping/lib/active_shipping/shipping/carrier.rb
+++ vendor/plugins/active_shipping/lib/active_shipping/shipping/carrier.rb
@@ -3,7 +3,7 @@ module ActiveMerchant
     class Carrier
       
       include RequiresParameters
-      include PostsData
+      include ActiveMerchant::Shipping::PostsData
       include Quantified
       
       attr_reader :last_request
