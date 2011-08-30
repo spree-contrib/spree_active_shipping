abf6f48bb74ca1c649cb186f7a242fc65c40d197
Tyler Rick <tyler@k3integrations.com>
2011-08-29 20:03:26 -0700

    Instead of individually naming all the calculators, just get the list of subclasses dynamically (Calculator::Fedex::Base.descendants, etc.)

--- lib/spree_active_shipping.rb
+++ lib/spree_active_shipping.rb
@@ -18,62 +18,19 @@ module ActiveShippingExtension
         Rails.env.production? ? require(c) : load(c)
       end
 
-      [
-        Calculator::Ups::Ground,
-        Calculator::Ups::NextDayAir,
-        Calculator::Ups::NextDayAirEarlyAm,
-        Calculator::Ups::NextDayAirSaver,
-        Calculator::Ups::Saver,
-        Calculator::Ups::SecondDayAir,
-        Calculator::Ups::ThreeDaySelect,
-        Calculator::Ups::WorldwideExpedited,
-        Calculator::Fedex::ExpressSaver,
-        Calculator::Fedex::FirstOvernight,
-        Calculator::Fedex::Ground,
-        Calculator::Fedex::GroundHomeDelivery,
-        Calculator::Fedex::InternationalEconomy,
-        Calculator::Fedex::InternationalEconomyFreight,
-        Calculator::Fedex::InternationalFirst,
-        Calculator::Fedex::InternationalGround,
-        Calculator::Fedex::InternationalPriority,
-        Calculator::Fedex::InternationalPriorityFreight,
-        Calculator::Fedex::InternationalPrioritySaturdayDelivery,
-        Calculator::Fedex::OneDayFreight,
-        Calculator::Fedex::OneDayFreightSaturdayDelivery,
-        Calculator::Fedex::PriorityOvernight,
-        Calculator::Fedex::PriorityOvernightSaturdayDelivery,
-        Calculator::Fedex::StandardOvernight,
-        Calculator::Fedex::ThreeDayFreight,
-        Calculator::Fedex::ThreeDayFreightSaturdayDelivery,
-        Calculator::Fedex::StandardOvernight,
-        Calculator::Fedex::ThreeDayFreight,
-        Calculator::Fedex::ThreeDayFreightSaturdayDelivery,
-        Calculator::Fedex::TwoDay,
-        Calculator::Fedex::TwoDayFreight,
-        Calculator::Fedex::TwoDayFreightSaturdayDelivery,
-        Calculator::Fedex::TwoDaySaturdayDelivery,
-        Calculator::Usps::MediaMail,
-        Calculator::Usps::ExpressMail,
-        Calculator::Usps::ExpressMailInternational,
-        Calculator::Usps::PriorityMail,
-        Calculator::Usps::PriorityMailSmallFlatRateBox,
-        Calculator::Usps::PriorityMailMediumFlatRateBox,
-        Calculator::Usps::PriorityMailLargeFlatRateBox,
-        Calculator::Usps::PriorityMailFlatRateEnvelope,
-        Calculator::Usps::PriorityMailInternational,
-        Calculator::Usps::PriorityMailInternationalSmallFlatRateBox,
-        Calculator::Usps::PriorityMailInternationalMediumFlatRateBox,
-        Calculator::Usps::PriorityMailInternationalLargeFlatRateBox,
-      ].each(&:register)
-      
+      (
+        Calculator::Fedex::Base.descendants +
+        Calculator::Ups::Base.descendants +
+        Calculator::Usps::Base.descendants
+      ).each(&:register)
+
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
-      
+
       #Only required until following active_shipping commit is merged (add negotiated rates).
       #http://github.com/BDQ/active_shipping/commit/2f2560d53aa7264383e5a35deb7264db60eb405a
       ActiveMerchant::Shipping::UPS.send(:include, Spree::ActiveShipping::UpsOverride)
-      
     end
 
     config.autoload_paths += %W(#{config.root}/lib)
