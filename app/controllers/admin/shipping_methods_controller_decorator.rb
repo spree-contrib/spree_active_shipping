bf72c96275779ff7ee2159477b5c3482848f6dc1
John Hwang <johnyhwang@gmail.com>
2012-02-06 12:45:45 -0500

    Fixing calculator collection for admin shippnig_method new/edit form.

--- app/controllers/admin/shipping_methods_controller_decorator.rb
+++ app/controllers/admin/shipping_methods_controller_decorator.rb
@@ -4,10 +4,6 @@ Admin::ShippingMethodsController.class_eval do
   # overriding to return an array of Calculator objects instead of a Set
   def load_data
     @available_zones = Zone.find :all, :order => :name
-    @calculators = []
-    ShippingMethod.calculators.each {|calc|
-      @calculators << eval(calc.name).new
-    }
-    @calculators = @calculators.sort_by(&:description)
+    @calculators = ShippingMethod.calculators.sort_by(&:description)
   end
 end
