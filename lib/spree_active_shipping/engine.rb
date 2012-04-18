9d4af80225664bfdf0583c7db10330ade1062e5a
Ryan Bigg <radarlistener@gmail.com>
2012-04-18 16:39:01 -0400

    Fix calculator require to be requiring in ascending order as well. Related to #26

--- lib/spree_active_shipping/engine.rb
+++ lib/spree_active_shipping/engine.rb
@@ -21,7 +21,7 @@ module SpreeActiveShippingExtension
     config.to_prepare &method(:activate).to_proc
 
     initializer "spree_active_shipping.register.calculators" do |app|
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/models/spree/calculator/**/*.rb")) do |c|
+      Dir[File.join(File.dirname(__FILE__), "../../app/models/spree/calculator/**/*.rb")].sort.each do |c|
         Rails.env.production? ? require(c) : load(c)
       end
 
