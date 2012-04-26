5f4cb995fe77f9e042f6fbe5f61cacead079aef4
Ryan Bigg <radarlistener@gmail.com>
2012-04-26 16:09:25 -0500

    Use correct inheritance for BogusCalculator

--- spec/lib/spree/bogus_calculator.rb
+++ spec/lib/spree/bogus_calculator.rb
@@ -2,7 +2,7 @@ module Spree
   module ActiveShipping
     # Bogus calcualtor for testing purposes.  Uses the common functionality of ActiveShippingCalcualtor but
     # doesn't actually use a real carrier to obtain rates.
-    class BogusCalculator < ActiveMerchant::Shipping::Base
+    class BogusCalculator < Spree::Calculator::ActiveShipping::Base
       def carrier
         Spree::ActiveShipping::BogusCarrier
       end
