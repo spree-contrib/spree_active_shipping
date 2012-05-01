35e872c015635f7b1178e07601db254db5fc6756
Denis Ivanov <visible.h4x@gmail.com>
2012-04-30 20:31:14 -0400

    Correcting handling of products with no weights (to take into account quantities)

--- app/models/spree/calculator/active_shipping/base.rb
+++ app/models/spree/calculator/active_shipping/base.rb
@@ -118,9 +118,13 @@ module Spree
         # Generates an array of Package objects based on the quantities and weights of the variants in the line items
         def packages(order)
           multiplier = Spree::ActiveShipping::Config[:unit_multiplier]
+          default_weight = Spree::ActiveShipping::Config[:default_weight]
+          
           weight = order.line_items.inject(0) do |weight, line_item|
-            weight + (line_item.variant.weight.present? ? (line_item.quantity * line_item.variant.weight * multiplier) : Spree::ActiveShipping::Config[:default_weight])
+            item_weight = line_item.variant.weight.present? ? line_item.variant.weight : default_weight
+            weight + (line_item.quantity * item_weight * multiplier)
           end
+          
           package = Package.new(weight, [], :units => Spree::ActiveShipping::Config[:units].to_sym)
           [package]
         end
