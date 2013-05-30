7ec5ef2116f4de68974be4472423ea87b7550a65
Zach Karpinski <zkarpinski@gmail.com>
2013-05-30 10:38:34 -0500

    Manually defined product package routes
    This solved #68 and removes route duplication for admin/products and
    admin/product/product_packages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.draw do
   namespace :admin do
     resource :active_shipping_settings, :only => ['show', 'update', 'edit']
-
-    resources :products do
-      resources :product_packages
-    end
   end
+
+  get 'admin/products/:product_id/product_packages(.:format)' => 'admin/product_packages#index', :as => 'admin_product_product_packages'
+  post 'admin/products/:product_id/product_packages(.:format)' => 'admin/product_packages#create'
+  get 'admin/products/:product_id/product_packages/new(.:format)' => 'admin/product_packages#new', :as => 'new_admin_product_product_package'
+  get 'admin/products/:product_id/product_packages/:id/edit(.:format)' => 'admin/product_packages#edit', :as => 'edit_admin_product_product_package'
+  get 'admin/products/:product_id/product_packages/:id(.:format)' => 'admin/product_packages#show', :as => 'admin_product_product_package'
+  put 'admin/products/:product_id/product_packages/:id(.:format)' => 'admin/product_packages#update'
+  delete 'admin/products/:product_id/product_packages/:id(.:format)' => 'admin/product_packages#destroy'
+
 end
+
+
