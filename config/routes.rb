d4d2d7fd80c5fa8dc978ee677d398c3e19532615
Ryan Bigg <radarlistener@gmail.com>
2013-11-06 10:27:41 +1100

    Check for add_routes and use that if it exists

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+routes = lambda do
   namespace :admin do
     resource :active_shipping_settings, :only => ['show', 'update', 'edit']
 
@@ -6,4 +6,10 @@ Spree::Core::Engine.add_routes do
       resources :product_packages
     end
   end
+end
+
+if Spree::Core::Engine.respond_to?(:add_routes)
+  Spree::Core::Engine.add_routes(&routes)
+else
+  Spree::Core::Engine.routes.draw(&routes)
 end
