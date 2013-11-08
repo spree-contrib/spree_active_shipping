20dd6e4154880f7ffd02eda99db6a3b9be2019ec
Nate Lowrie <nate@finelineautomation.com>
2013-11-08 13:11:55 -0500

    Redid the product packages admin interface. Brought it up to the new admin standard from 1.3.
    The new and edit commands function like the images new command.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,5 +3,5 @@ Spree::Product.class_eval do
   has_many :product_packages, :dependent => :destroy
 
   attr_accessible :product_packages_attributes
-  accepts_nested_attributes_for :product_packages, :allow_destroy => true, :reject_if => lambda { |pp| pp[:weight].blank? or Integer(pp[:weight]) < 1 }
+  accepts_nested_attributes_for :product_packages, :reject_if => lambda { |pp| pp[:weight].blank? or Integer(pp[:weight]) < 1 }
 end
