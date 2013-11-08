1ed5a6cb96059c033652468f45efdce04ab6290a
Nate Lowrie <nate@finelineautomation.com>
2013-11-08 14:14:46 -0500

    Updated the settings pages to the new UI standard introduced in Spree 1.3.
    Merge wasn't clean again due to spree translations.
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>
    
    Conflicts:
    	app/overrides/add_active_shipping_settings_tab.rb
    	app/views/spree/admin/active_shipping_settings/edit.html.erb
    	app/views/spree/admin/active_shipping_settings/show.html.erb
    	config/locales/en.yml

--- app/controllers/spree/admin/active_shipping_settings_controller.rb
+++ app/controllers/spree/admin/active_shipping_settings_controller.rb
@@ -1,10 +1,14 @@
 class Spree::Admin::ActiveShippingSettingsController < Spree::Admin::BaseController
 
-  def show
-    @config = Spree::ActiveShippingConfiguration.new
-  end
-
   def edit
+    @preferences_UPS = [:ups_login, :ups_password, :ups_key, :shipper_number]
+    @preferences_FedEx = [:fedex_login, :fedex_password, :fedex_account, :fedex_key]
+    @preferences_USPS = [:usps_login]
+    @preferences_CanadaPost = [:canada_post_login]
+    @preferences_OriginAddress = [:origin_country, :origin_state, :origin_city, :origin_zip]
+    @preferences_GeneralSettings = [:units, :unit_multiplier, :default_weight, :handling_fee, 
+      :max_weight_per_package, :test_mode]
+
     @config = Spree::ActiveShippingConfiguration.new
   end
 
@@ -16,7 +20,7 @@ class Spree::Admin::ActiveShippingSettingsController < Spree::Admin::BaseControl
       config[name] = value
     end
 
-    redirect_to admin_active_shipping_settings_path
+    redirect_to edit_admin_active_shipping_settings_path
   end
 
 end
