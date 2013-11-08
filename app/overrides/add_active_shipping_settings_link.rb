3948f5d4aa4ed0025d42b52755874ffb008a748c
Nate Lowrie <nate@finelineautomation.com>
2013-11-08 14:14:46 -0500

    Updated the settings pages to the new UI standard introduced in Spree 1.3.

--- app/overrides/add_active_shipping_settings_link.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                     :name => "add_active_shipping_settings_link",
-                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :text => "<tr>
-      <td><%= link_to \"Active Shipping\", admin_active_shipping_settings_path %></td>
-      <td><%= \"Configure Active Shipping Settings\" %></td>
-      </tr>")
