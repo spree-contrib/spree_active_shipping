b8cb9db6add6480e201ebbd937f789df7e75773e
Nate Lowrie <nate@finelineautomation.com>
2013-11-08 13:11:55 -0500

    Redid the product packages admin interface. Brought it up to the new admin standard from 1.3.
    The new and edit commands function like the images new command.
    
    Wasn't a clean merge because of the Spree 2.0 translations
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>
    
    Conflicts:
    	app/controllers/spree/admin/product_packages_controller.rb
    	app/models/spree/product_decorator.rb
    	app/views/spree/admin/product_packages/index.html.erb
    	config/locales/en.yml

---
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -0,0 +1,12 @@
+Spree::Admin::ProductsController.class_eval do
+  def product_packages
+    @product = Spree::Product.find_by_permalink!(params[:id])
+    @packages = @product.product_packages
+    @product.product_packages.build
+    
+    respond_with(@object) do |format|
+    format.html { render :layout => !request.xhr? }
+      format.js { render :layout => false }
+    end
+  end
+end
