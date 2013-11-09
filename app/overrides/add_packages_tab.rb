6dffab45ecc2c7abe70287278f399bb40c55eb33
Nate Lowrie <nate@finelineautomation.com>
2013-11-09 08:08:27 -0500

    Added an icon to the product packages tab entry.
    I think the truck fits better than a barcode.
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>
    
    Conflicts:
    	app/overrides/add_packages_tab.rb

--- app/overrides/add_packages_tab.rb
+++ app/overrides/add_packages_tab.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "add_product_packages_tab",
                      :insert_bottom => "[data-hook='admin_product_tabs']",
                      :text => "<li<%== ' class=\"active\"' if current == 'Product Packages' %>>
-                       <%= link_to Spree.t(:product_packages), admin_product_product_packages_url(@product), :class => 'icon-barcode' %>
+                       <%= link_to Spree.t(:product_packages), admin_product_product_packages_url(@product), :class => 'icon-truck' %>
                      </li>")
