dc048c9a6927847abb964046a38e83298325a0b3
Nate Lowrie <nate@finelineautomation.com>
2013-11-09 08:08:27 -0500

    Added an icon to the product packages tab entry.

--- app/overrides/add_packages_tab.rb
+++ app/overrides/add_packages_tab.rb
@@ -2,5 +2,6 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "add_product_packages_tab",
                      :insert_bottom => "[data-hook='admin_product_tabs']",
                      :text => "<li<%== ' class=\"active\"' if current == 'Product Packages' %>>
-                       <%= link_to t(:product_packages), admin_product_product_packages_url(@product) %>
+                       <%= link_to_with_icon 'icon-truck', t(:product_packages), admin_product_product_packages_url(@product) %>
                      </li>")
+
