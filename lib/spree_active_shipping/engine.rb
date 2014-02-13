b25a9444363a9c26ede54891002b02a05ea4a100
Andrew Ryan Lazarus <nerdrew@gmail.com>
2014-02-13 11:06:01 -0800

    Fix for already initialized constant errors
    Fixes #176
    Fixes #147
    Fixes #75

--- lib/spree_active_shipping/engine.rb
+++ lib/spree_active_shipping/engine.rb
@@ -28,16 +28,13 @@ module SpreeActiveShippingExtension
     config.to_prepare &method(:activate).to_proc
 
     initializer "spree_active_shipping.register.calculators" do |app|
-      Dir[File.join(File.dirname(__FILE__), "../../app/models/spree/calculator/**/*.rb")].sort.each do |c|
-        Rails.env.production? ? require(c) : load(c)
+      classes = Dir.chdir File.join(File.dirname(__FILE__), "../../app/models") do
+        Dir["spree/calculator/**/*.rb"].reject {|path| path =~ /base.rb$/ }.map do |path|
+          path.gsub('.rb', '').camelize.constantize
+        end
       end
 
-      app.config.spree.calculators.shipping_methods.concat(
-        Spree::Calculator::Shipping::Fedex::Base.descendants +
-        Spree::Calculator::Shipping::CanadaPost::Base.descendants +
-        Spree::Calculator::Shipping::Ups::Base.descendants +
-        Spree::Calculator::Shipping::Usps::Base.descendants
-      )
+      app.config.spree.calculators.shipping_methods.concat classes
     end
 
         # sets the manifests / assets to be precompiled, even when initialize_on_precompile is false
