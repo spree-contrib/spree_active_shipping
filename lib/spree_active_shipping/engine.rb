8242a8f7a50148bb20ce534836a6a5aeac34be73
Andrew Ryan Lazarus <nerdrew@gmail.com>
2014-03-20 17:12:13 -0700

    Guard against missing shipping_method calculators
    Fixes #176
    Fixes #174
    Fixes #75

--- lib/spree_active_shipping/engine.rb
+++ lib/spree_active_shipping/engine.rb
@@ -28,13 +28,15 @@ module SpreeActiveShippingExtension
     config.to_prepare &method(:activate).to_proc
 
     initializer "spree_active_shipping.register.calculators" do |app|
-      classes = Dir.chdir File.join(File.dirname(__FILE__), "../../app/models") do
-        Dir["spree/calculator/**/*.rb"].reject {|path| path =~ /base.rb$/ }.map do |path|
-          path.gsub('.rb', '').camelize.constantize
+      if app.config.spree.calculators.shipping_methods
+        classes = Dir.chdir File.join(File.dirname(__FILE__), "../../app/models") do
+          Dir["spree/calculator/**/*.rb"].reject {|path| path =~ /base.rb$/ }.map do |path|
+            path.gsub('.rb', '').camelize.constantize
+          end
         end
-      end
 
-      app.config.spree.calculators.shipping_methods.concat classes
+        app.config.spree.calculators.shipping_methods.concat classes
+      end
     end
 
         # sets the manifests / assets to be precompiled, even when initialize_on_precompile is false
