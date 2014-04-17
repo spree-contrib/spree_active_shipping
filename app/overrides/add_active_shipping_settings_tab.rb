79f719e688ede5a8338ab88fde237495979b4d08
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-04-17 12:43:58 -0400

    Update overrides to use deface extension and add a cancan check for editing product packages.

--- app/overrides/add_active_shipping_settings_tab.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "add_active_shipping_settings_link",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     :text => "<%= configurations_sidebar_menu_item t(:active_shipping_settings), edit_admin_active_shipping_settings_path %>")
