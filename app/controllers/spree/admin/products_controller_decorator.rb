afe61e2bde2a11ad762b763eecfdfb4784333a86
Jorge Pardiñas <jparbros@gmail.com>
2014-05-14 13:41:01 -0500

    Searching by slug instead by permalink

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,9 +1,9 @@
 Spree::Admin::ProductsController.class_eval do
   def product_packages
-    @product = Spree::Product.find_by_permalink!(params[:id])
+    @product = Spree::Product.find_by_slug!(params[:id])
     @packages = @product.product_packages
     @product.product_packages.build
-    
+
     respond_with(@object) do |format|
     format.html { render :layout => !request.xhr? }
       format.js { render :layout => false }
