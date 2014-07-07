8a33b2418426fa7323085ed7870668d0894f0fcd
Ryan Bigg <radarlistener@fastmail.fm>
2014-07-07 13:57:31 +1000

    Use slug for products, not permalink
    Fixes #193

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Admin::ProductsController.class_eval do
   def product_packages
-    @product = Spree::Product.find_by_permalink!(params[:id])
+    @product = Spree::Product.find_by_slug!(params[:id])
     @packages = @product.product_packages
     @product.product_packages.build
     
