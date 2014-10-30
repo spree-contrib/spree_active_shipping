98a6a3644f31434b0304dbacd4c5453a23887d4b
Marc Leglise <mleglise@gmail.com>
2014-10-30 11:50:12 -0700

    Update `weight_limits_spec` for `Spree::Stock::Package` now expecting inventory_units inside of new class `Spree::Stock::ContentItem`
    6 specs still failing, centered around `Spree::Calculator::Shipping::ActiveShipping::Base.convert_package_to_weights_array`

--- spec/models/weight_limits_spec.rb
+++ spec/models/weight_limits_spec.rb
@@ -4,11 +4,21 @@ include ActiveMerchant::Shipping
 module ActiveShipping
   describe Spree::ShippingCalculator do
 
+    def build_content_items(variant, quantity, order)
+      quantity.times.map {|i| Spree::Stock::ContentItem.new(build_inventory_unit(variant, order)) }
+    end
+
+    def build_inventory_unit(variant, order)
+      build(:inventory_unit, variant: variant, order: order)
+    end
+
     let(:country) { mock_model Spree::Country, :iso => "CA", :state_name => "Quebec", :state => nil }
     let(:address) { mock_model Spree::Address, :country => country, :state_name => country.state_name, :city => "Montreal", :zipcode => "H2B", :state => nil }
     let(:usa) { FactoryGirl.create(:country, :name => "USA", :iso => "US") }
     let(:state) { FactoryGirl.create(:state, country: usa, abbr: 'MD', name: 'Maryland')}
     let(:us_address) { FactoryGirl.create(:address, :country => usa, :state => state, :city => "Chevy Chase", :zipcode => "20815") }
+    let(:ca_order) { mock_model(Spree::Order, :ship_address => address) }
+    let(:us_order) { mock_model(Spree::Order, :ship_address => us_address) }
     let(:package1) { mock_model(Spree::ProductPackage, :length => 12, :width => 24, :height => 47, :weight => 36) }
     let(:package2) { mock_model(Spree::ProductPackage, :length => 6, :width => 6, :height => 51, :weight => 43) }
     let(:variant1) { build(:variant, :weight => 20.0) }
@@ -17,45 +27,45 @@ module ActiveShipping
     let(:variant4) { build(:variant, :weight => 100.0) }
     let(:variant5) { build(:variant, :weight => 0) }
     let(:variant6) { build(:variant, :weight => -1.0) }
-    let(:variant7) { double(Spree::Variant, :weight => 29.0, :product => mock_model(Spree::Product, :product_packages => [package1, package2])) }
-    let(:variant8) { double(Spree::Variant, :weight => 5.25, :product => mock_model(Spree::Product, :product_packages => [])) }
+    let(:variant7) { mock_model(Spree::Variant, :weight => 29.0, :product => mock_model(Spree::Product, :product_packages => [package1, package2])) }
+    let(:variant8) { mock_model(Spree::Variant, :weight => 5.25, :product => mock_model(Spree::Product, :product_packages => [])) }
     let(:california) { FactoryGirl.create(:state, country: usa, abbr: 'CA', name: 'California') }
     let(:stock_location) { FactoryGirl.create(:stock_location, :address1 => '1313 S Harbor Blvd', :address2 => '', :city => 'Anaheim', :state => california, :country => usa, :phone => '7147814000', :active => 1) }
 
     let(:package) { double(Spree::Stock::Package,
-          order: mock_model(Spree::Order, :ship_address => address),
-          contents: [Spree::Stock::Package::ContentItem.new(nil, variant1, 10),
-                    Spree::Stock::Package::ContentItem.new(nil, variant2, 4),
-                    Spree::Stock::Package::ContentItem.new(nil, variant3, 1)]) }
+          order: ca_order,
+          contents: [build_content_items(variant1, 10, ca_order),
+                    build_content_items(variant2, 4, ca_order),
+                    build_content_items(variant3, 1, ca_order)].flatten) }
 
     let(:too_heavy_package) do
       Spree::Stock::Package.extend ActiveModel::Naming
       mock_model(
         Spree::Stock::Package,
-        order: mock_model(Spree::Order, :ship_address => address),
+        order: ca_order,
         stock_location: stock_location,
         contents: [
-          Spree::Stock::Package::ContentItem.new(nil, variant3, 2),
-          Spree::Stock::Package::ContentItem.new(nil, variant4, 2)
-        ]
+          build_content_items(variant3, 2, ca_order),
+          build_content_items(variant4, 2, ca_order)
+        ].flatten
       )
     end
 
     let(:us_package) { double(Spree::Stock::Package,
-          order: mock_model(Spree::Order, :ship_address => us_address),
-          contents: [Spree::Stock::Package::ContentItem.new(nil, variant1, 10),
-                    Spree::Stock::Package::ContentItem.new(nil, variant2, 4),
-                    Spree::Stock::Package::ContentItem.new(nil, variant3, 1)]) }
+          order: ca_order,
+          contents: [build_content_items(variant1, 10, ca_order),
+                    build_content_items(variant2, 4, ca_order),
+                    build_content_items(variant3, 1, ca_order)].flatten) }
 
     let(:package_with_invalid_weights) { double(Spree::Stock::Package,
-          order: mock_model(Spree::Order, :ship_address => us_address),
-          contents: [Spree::Stock::Package::ContentItem.new(nil, variant5, 1),
-                    Spree::Stock::Package::ContentItem.new(nil, variant6, 1)]) }
+          order: us_order,
+          contents: [build_content_items(variant5, 1, us_order),
+                    build_content_items(variant6, 1, us_order)].flatten) }
 
     let(:package_with_packages) { double(Spree::Stock::Package,
-          order: mock_model(Spree::Order, :ship_address => us_address),
-          contents: [Spree::Stock::Package::ContentItem.new(nil, variant8, 4),
-                    Spree::Stock::Package::ContentItem.new(nil, variant7, 2)]) }
+          order: us_order,
+          contents: [build_content_items(variant8, 4, us_order),
+                    build_content_items(variant7, 2, us_order)].flatten) }
 
     let(:international_calculator) {  Spree::Calculator::Shipping::Usps::PriorityMailInternational.new }
     let(:domestic_calculator) {  Spree::Calculator::Shipping::Usps::PriorityMail.new }
