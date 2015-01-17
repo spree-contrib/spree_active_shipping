37311c90ba9ad0012039dd91cd2993e19d19f024
Pete Campbell <pete@sumirolabs.com>
2015-01-16 21:53:12 -0500

    Bugfix - add the SpreeActiveShipping shipping methods *after* the core shipping methods are added

--- lib/spree_active_shipping/engine.rb
+++ lib/spree_active_shipping/engine.rb
@@ -27,7 +27,7 @@ module SpreeActiveShippingExtension
     config.autoload_paths += %W(#{config.root}/lib)
     config.to_prepare &method(:activate).to_proc
 
-    initializer "spree_active_shipping.register.calculators" do |app|
+    initializer "spree_active_shipping.register.calculators", after: "spree.register.calculators" do |app|
       if app.config.spree.calculators.shipping_methods
         classes = Dir.chdir File.join(File.dirname(__FILE__), "../../app/models") do
           Dir["spree/calculator/**/*.rb"].reject {|path| path =~ /base.rb$/ }.map do |path|
