cb52b70fbadf06dd01173483d314868432469a18
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-04-08 13:10:00 -0500

    start upgrades on active_shipping 1.0
    upgrade active_shipping overrides
    
    fix missing do
    
    fixed constants
    
    update get_text for nokogiri::xml
    
    cleanup logs... :o
    
    fixes #213

--- lib/spree/active_shipping/canada_post_override.rb
+++ lib/spree/active_shipping/canada_post_override.rb
@@ -18,28 +18,30 @@ module Spree
           # <!--   - ready to ship (optional)     -->
           
           def build_line_items(line_items)
-            xml_line_items = XmlNode.new('lineItems') do |line_items_node|
-              
-              line_items.each do |line_item|
-                
-                line_items_node << XmlNode.new('item') do |item|
-                  item << XmlNode.new('quantity', 1) 
-                  item << XmlNode.new('weight', line_item.kilograms)
-                  item << XmlNode.new('length', line_item.cm(:length).to_s)
-                  item << XmlNode.new('width', line_item.cm(:width).to_s)
-                  item << XmlNode.new('height', line_item.cm(:height).to_s)
-                  item << XmlNode.new('description', line_item.options[:description] || ' ')
-                  # Only this line got changed
-                  item << XmlNode.new('readyToShip') if line_item.options[:ready_to_ship]
-                  
-                  # By setting the 'readyToShip' tag to true, Sell Online will not pack this item in the boxes defined in the merchant profile.
+            xml_builder = Nokogiri::XML::Builder.new do |xml|
+              xml.lineItems do
+
+                line_items.each do |line_item|
+                  xml.item do
+                    xml.quantity(1) 
+                    xml.weight(line_item.kilograms)
+                    xml.length(line_item.cm(:length).to_s)
+                    xml.width(line_item.cm(:width).to_s)
+                    xml.height(line_item.cm(:height).to_s)
+                    xml.description(line_item.options[:description] || ' ')
+                    xml.readyToShip if line_item.options[:ready_to_ship] # SPREE OVERRIDE
+                    
+                    # By setting the 'readyToShip' tag to true, Sell Online will not pack this item in the boxes defined in the merchant profile.
+                  end
                 end
               end
             end
             
-            xml_line_items
+            xml_builder.to_xml
           end
+
         end
+
       end
     end
   end
