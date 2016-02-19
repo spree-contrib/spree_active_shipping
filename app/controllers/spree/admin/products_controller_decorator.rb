669f6fe81703222eb436b8da21b24661252cfa90
Tanmay Sinha <tanmay3011@gmail.com>
2016-02-19 11:00:12 +0530

    Update ruby syntax

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,12 +1,12 @@
 Spree::Admin::ProductsController.class_eval do
   def product_packages
-    @product = Spree::Product.find_by_slug!(params[:id])
+    @product  = Spree::Product.find_by_slug!(params[:id])
     @packages = @product.product_packages
     @product.product_packages.build
-    
+
     respond_with(@object) do |format|
-    format.html { render :layout => !request.xhr? }
-      format.js { render :layout => false }
+      format.html { render layout: !request.xhr? }
+      format.js { render layout: false }
     end
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 # Add product packages relation
 Spree::Product.class_eval do
-  has_many :product_packages, :dependent => :destroy
+  has_many :product_packages, dependent: :destroy
 
-  accepts_nested_attributes_for :product_packages, :allow_destroy => true, :reject_if => lambda { |pp| pp[:weight].blank? or Integer(pp[:weight]) < 1 }
+  accepts_nested_attributes_for :product_packages, allow_destroy: true, reject_if: lambda { |pp| (pp[:weight].blank? || (Integer(pp[:weight]) < 1)) }
 end
