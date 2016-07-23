ebd66b691fc9ceb2bc548e514272e9d5f53b43e6
Johann Boutet <johannboutet@gmail.com>
2016-07-23 12:13:44 -0400

    Reformat indentation to 2 spaces.

--- app/models/spree/calculator/shipping/canada_post/xpresspost_usa.rb
+++ app/models/spree/calculator/shipping/canada_post/xpresspost_usa.rb
@@ -1,11 +1,11 @@
 module Spree
-    module Calculator::Shipping
-        module CanadaPost
-            class XpresspostUsa < Spree::Calculator::Shipping::CanadaPost::Base
-                def self.description
-                    I18n.t("canada_post.xpresspost_usa")
-                end
-            end
+  module Calculator::Shipping
+    module CanadaPost
+      class XpresspostUsa < Spree::Calculator::Shipping::CanadaPost::Base
+        def self.description
+          I18n.t("canada_post.xpresspost_usa")
         end
+      end
     end
+  end
 end
